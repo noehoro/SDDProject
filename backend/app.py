c0f230aadc60a14586159e5c8f4bb99c20db9ac3
Michael Anderson <michael.j.anderson@me.com>
2022-11-06 23:56:54 -0500

    ironed out login bug + added remember + added logout
    login should be fully functional and completed now

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, send_file
 from helpers.database import db, Site, User, Machine, Activity
 from qr_generator import qr_code, create_code
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from datetime import *
 from classes.auth import Auth
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'laundry-manager'
+
 
 db.app = app
 db.init_app(app)
 with app.app_context():
     db.create_all()
 app.register_blueprint(qr_code)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+@login_manager.user_loader
+def get_user(userid):
+    return User.query.get(int(userid))
 
 @app.route("/register", methods=['POST'])
 def register():
@@ -37,7 +47,7 @@ def register():
 
 
         db.session.add(new_user)
-        db.session.add(site_post)
+        db.session.add(new_site)
 
         db.session.commit()
 
@@ -55,14 +65,18 @@ def login():
         password = request.args.get('password')
 
         user = User.query.filter_by(username=username).first()
-
         if user:
-            if AuthAgent.verify_key(password, user.password):
+            if AuthAgent.verify_key(user.password, password):
                 response['loggedin'] = 1
+                login_user(user, remember=True)
                 return response
+            else:
+                response['pass'] = 1
+                return response  
 
         else:
-            return request
+            response['user'] = 1
+            return response
 
 
 
@@ -91,6 +105,13 @@ def run_machine():
 
     machine_id = request.args['machine']
     return str(Machine.query.get(machine_id).time)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return {'loggedout': 1}
+
 
 @app.route("/")
 def homepage():
