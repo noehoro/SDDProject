099ebc26689cab82d4d499747a56d542c68ea1fc
Chris He <h1119623520@gmail.com>
2022-11-10 22:04:33 -0500

    management page frontend sorta done

--- frontendV2/Resource/js/script.js
+++ frontendV2/Resource/js/script.js
@@ -1,10 +1,17 @@
-function increase() {
+function increase(perc) {
+    //change status bar fill percentage
+    let bars = document.getElementsByClassName("bar");
+    for(let i = 0; i < bars.length; i++){
+        bars[i].style.setProperty('--m',perc[i].toString()+"%");
+    }
+    
     // Change the variable to modify the speed of the number increasing from 0 to (ms)
-    let SPEED = 40;
+    let SPEED = 30;
     // Retrieve the percentage value
     let limitList = [];
     let barList = document.getElementsByClassName("value1");
     for(let i = 0; i < barList.length; i++){
+        barList[i].innerHTML = perc[i];
         parseInt(limitList.push(barList[i].innerHTML),10);
     }
 
@@ -17,4 +24,4 @@ function increase() {
     }
 }
 
-increase();
+increase([7,78,8]);
