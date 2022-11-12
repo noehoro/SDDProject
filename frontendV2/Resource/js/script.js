68f42f178d73b6892b154762b051f493aedd7c09
Chris He <h1119623520@gmail.com>
2022-11-12 15:18:42 -0500

    2

---
+++ frontendV2/Resource/js/script.js
@@ -0,0 +1,40 @@
+function increase(perc) {
+    //change status bar fill percentage
+    let bars = document.getElementsByClassName("bar");
+    for(let i = 0; i < bars.length; i++){
+        bars[i].style.setProperty('--m',perc[i].toString()+"%");
+    }
+    
+    // Change the variable to modify the speed of the number increasing from 0 to (ms)
+    let SPEED = 30;
+    // Retrieve the percentage value
+    let limitList = [];
+    let barList = document.getElementsByClassName("value1");
+    for(let i = 0; i < barList.length; i++){
+        barList[i].innerHTML = perc[i];
+        parseInt(limitList.push(barList[i].innerHTML),10);
+    }
+
+    for(let j = 0; j < limitList.length; j++){
+        for(let i = 0; i <= limitList[j]; i++) {
+            setTimeout(function () {
+                document.getElementsByClassName("value1")[j].innerHTML = i + "%";
+            }, SPEED * i);
+        }
+    }
+}
+
+increase([7,78,8]);
+
+
+function func12(){
+    $.ajax({
+        type: "GET",
+        // data: {info: info},
+        url:"http://127.0.0.1:5000/",
+        success:function(msg){
+            console.log(msg);
+        }
+    })
+}
+
