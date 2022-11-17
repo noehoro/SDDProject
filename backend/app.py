62169a70c93d5dbae62269a156b8cce92ccd90f5
Michael Anderson <michael.j.anderson@me.com>
2022-11-17 15:58:55 -0500

    OOP design changes

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,51 @@
 from flask import Flask, render_template, request, send_file
 from helpers.database import db, Site, User, Machine, Activity
-from qr_generator import qr_code, create_code
+from classes.qr_generator import QRBlueprint
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from classes.auth import Auth
 from datetime import *
 import time
-from classes.auth import Auth
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'login-manager-admin'
+class Framework:
+    def __init__(self):
 
-db.app = app
-db.init_app(app)
-with app.app_context():
-    db.create_all()
-app.register_blueprint(qr_code)
+        self.login_manager = LoginManager()
+        self.login_manager.login_view = 'login'
+        self.app = Flask(__name__)
 
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
 
-@login_manager.user_loader
-def get_user(userid):
-    return User.query.get(int(userid))
+class App:
 
-@app.route("/register", methods=['POST'])
-def register():
+    def __init__(self, Frame, host, port, appopt=None, manager=None):
 
-    if request.method == "POST":
+        self.manager = manager
+        self.application = appopt
+
+        login_manager = Frame.login_manager
+
+        self.app = Frame.app
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+        self.app.config['SECRET_KEY'] = 'login-manager-admin'
+
+        self.db = db
+        self.db.app = self.app
+        self.db.init_app(self.app)
+
+        with self.app.app_context():
+            self.db.create_all()
+
+        login_manager.init_app(self.app)
+
+        self.host = host
+        self.port = port
+
+    def register(self, passwrd):
+
         response = {'exists':0, 'success': 0} 
         AuthAgent = Auth()
 
-        username = request.args.get('username')
-        
-        password = AuthAgent.hash_key(request.args.get('password'))
-        site_post = request.args.get('site')
-
+        password = AuthAgent.hash_key(passwrd)
 
         if User.query.filter_by(username=username).first():
             response['exists'] = 1
@@ -47,24 +56,20 @@ def register():
         new_site = Site(name=site_post)
 
 
-        db.session.add(new_user)
-        db.session.add(new_site)
+        self.db.session.add(new_user)
+        self.db.session.add(new_site)
 
-        db.session.commit()
+        self.db.session.commit()
 
         response['success'] = 0
 
         return response
 
-@app.route('/login', methods=['POST'])
-def login():
+    def login(self, username, password):
 
-    if request.method == 'POST':
+        
         response = {"user": 0, "pass": 0, "loggedin": 0}
         AuthAgent = Auth()
-        username = request.args.get('username')
-        password = request.args.get('password')
-
         user = User.query.filter_by(username=username).first()
         if user:
             if AuthAgent.verify_key(user.password, password):
@@ -79,105 +84,169 @@ def login():
             response['user'] = 1
             return response
 
-@app.route('/new-machine', methods=['POST'])
-@login_required
-def new_machine():
+    def new_machine(self, time, site, machine_type):
 
-    if request.method == 'POST':
+        if request.method == 'POST':
 
-        time = request.args['time']
-        site = request.args['site']
-        machine_type = request.args['type']
+            switch = {'wash': '1', 'dry': '2', 'other': '3'}
 
-        switch = {'wash': '1', 'dry': '2', 'other': '3'}
+            machine_id = str(switch[machine_type]) + str(datetime.now().timestamp()).replace('.','')
+            
+            new_machine = Machine(id=machine_id, time=time, site=site)
 
+            QRCreator = QRBlueprint(is_new=1, code_int=machine_id)
+            route = QRCreator.create_code()
 
-        machine_id = str(switch[machine_type]) + str(datetime.now().timestamp()).replace('.','')
+            self.db.session.add(new_machine)
+            self.db.session.commit()
+
+            return send_file(route, mimetype='image/png')
+
+        else:
+            return 'new_machine is a POST only endpoint'
+
+    def run_machine(self, machine_id):
         
-        new_machine = Machine(id=machine_id, time=time, site=site)
-        route = create_code(is_new=1, code_int=machine_id)
+        if Activity.query.filter_by(id=machine_id).first():
+            return {'already_run': 1}
 
-        db.session.add(new_machine)
-        db.session.commit()
+        machine = Machine.query.filter_by(id=machine_id).first()
+        self.db.session.add(Activity(id=machine.id, time=time.time(), site=machine.site))
+        self.db.session.commit()
+        return {'machine_time': str(Machine.query.get(machine_id).time)}
 
-        return send_file(route, mimetype='image/png')
-    else:
-        return 'new_machine is a POST only endpoint'
+    def logout(self):
+        logout_user()
+        return {'loggedout': 1}
 
 
+    def dashboard(self, arg_site):
 
-@app.route("/run-machine", methods=['POST'])
-def run_machine():
+        requested_site = arg_site
 
-    machine_id = request.args['machine']
+        if not Site.query.filter_by(name=requested_site).first():
+            return {'site':'null'}
+
+        machines = Machine.query.filter_by(site=requested_site)
+
+        form = {'site': requested_site}
+        for i in machines:
+
+            j = Activity.query.filter_by(id=i.id).first()
+
+            if j:
+                if int(time.time() - j.time) >= int(j.time):
+                    Activity.query.filter_by(id=j.id).delete()
+                    self.db.session.commit()
+                else:    
+                    temp_machine = Machine.query.filter_by(id=j.id).first()
+                    form[str(j.id)] = str(temp_machine.time + int(j.time - time.time()))
+            else:
+                form[str(i.id)] = 0
+        return form
+
+    def getsite(self):
+        return {'site': current_user.site}
+
+    def check_login(self):
+
+        response = {'loggedin':0}
+
+        try:
+            a = current_user.username
+            response['loggedin'] = 1
+            return reponse
+
+        except:
+            return response
+
+    def homepage(self):
+
+        # we can through some data return here so that we can display
+        # messages on our 
+        info_out = dict()
+        info_out['title'] = 'Laundry Manager'
+        info_out['ver']= 'beta_v1.0'
+
+        return info_out
+
+    def start(self):
+        self.app.run(host=self.host, port=self.port)
+
+
+class AppWrapper:
+
+    def __init__(self, start=0):
+        self.server = self.routes(start)
+
+    def routes(self, start):
+
+        flask_framework = Framework()
+        app = flask_framework.app
+        login_manager = flask_framework.login_manager
+
+        self.app_router = App(flask_framework, '0.0.0.0', '5000')
+
+        @app.route('/')
+        def homepage():
+            return self.app_router.homepage()
+
+        @app.route('/loggedin')
+        def check_login():
+            return self.app_router.check_login()
+
+        @app.route('/getsite')
+        @login_required
+        def getsite():
+            return self.app_router.getsite()
+
+        @app.route('/dashboard', methods=['POST'])
+        def dashboard():
+            if request.method == "POST":
+                return self.app_router.dashboard(request.args['site'])
+
+        @app.route("/logout")
+        @login_required
+        def logout():
+            return self.app_router.logout()
+
+        @app.route("/run-machine", methods=['POST'])
+        def run_machine():
+            if request.method == "POST":
+                return self.app_router.run_machine(request.args['machine'])
+
+        @app.route('/new-machine', methods=['POST'])
+        @login_required
+        def new_machine():
+
+            if request.method == "POST":
+                return self.app_router.new_machine(request.args['time'], request.args['site'], request.args['type'])
+
+        @app.route('/login', methods=['POST'])
+        def login():
+            if request.method == "POST":
+                username = request.args.get('username')
+                password = request.args.get('password')
+                return self.app_router.login(username, password)
+
+        @app.route("/register", methods=['POST'])
+        def register():
+
+            if request.method == "POST":
+
+                username = request.args.get('username')
+                password = AuthAgent.hash_key(request.args.get('password'))
+                site_post = request.args.get('site')
+                return self.app_router.register(username, password, site_post)
+
+        @login_manager.user_loader
+        def get_user(userid):
+            return User.query.get(int(userid))
+
+        if start:
+            app.run()
     
-    if Activity.query.filter_by(id=machine_id).first():
-        return {'already_run': 1}
 
-    machine = Machine.query.filter_by(id=machine_id).first()
-    db.session.add(Activity(id=machine.id, time=time.time(), site=machine.site))
-    db.session.commit()
-    return {'machine_time': str(Machine.query.get(machine_id).time)}
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return {'loggedout': 1}
-
-
-@app.route('/dashboard', methods=['POST'])
-def dashboard():
-
-    requested_site = request.args['site']
-
-    if not Site.query.filter_by(name=requested_site).first():
-        return {'site':'null'}
-
-    machines = Activity.query.filter_by(site=requested_site)
-
-    form = {'site': requested_site}
-    for i in machines:
-
-        if int(time.time() - i.time) >= int(i.time):
-            Activity.query.filter_by(id=i.id).delete()
-            db.session.commit()
-        else:    
-            temp_machine = Machine.query.filter_by(id=i.id).first()
-            form[str(i.id)] = str(temp_machine.time + int(i.time - time.time()))
-
-    return form
-
-@app.route('/getsite')
-def getsite():
-    return {'site': current_user.site}
-
-
-@app.route('/loggedin')
-def check_login():
-
-    response = {'loggedin':0}
-
-    try:
-        a = current_user.username
-        response['loggedin'] = 1
-        return reponse
-
-    except:
-        return response
-
-
-@app.route("/")
-def homepage():
-
-    # we can through some data return here so that we can display
-    # messages on our 
-    info_out = dict()
-    info_out['title'] = 'Laundry Manager'
-    info_out['ver']= 'beta_v1.0'
-
-    return info_out
 
 if __name__ == "__main__":
-    app.run()
+    application = AppWrapper(start=True)
