d6b988c9c3b0121bb4e2515bc417558dcc6dc7f5
Chris He <h1119623520@gmail.com>
2022-11-21 14:44:10 -0500

    new_machine bug

--- frontendV2/Resource/js/script.js
+++ frontendV2/Resource/js/script.js
@@ -51,36 +51,6 @@ let bgModel =
 '</div>'+
 
 $('#header_container').append(header);
-console.log("here");
 $('#footer_container').append(footer);
 $('#bg-model').append(bgModel);
 
-// ---------------------------------------------------------------------------------------------
-// Animation for status bar
-function increase(perc) {
-    //change status bar fill percentage
-    let bars = document.getElementsByClassName("bar");
-    for(let i = 0; i < bars.length; i++){
-        bars[i].style.setProperty('--m',perc[i].toString()+"%");
-    }
-    
-    // Change the variable to modify the speed of the number increasing from 0 to (ms)
-    let SPEED = 30;
-    // Retrieve the percentage value
-    let limitList = [];
-    let barList = document.getElementsByClassName("value1");
-    for(let i = 0; i < barList.length; i++){
-        barList[i].innerHTML = perc[i];
-        parseInt(limitList.push(barList[i].innerHTML),10);
-    }
-
-    for(let j = 0; j < limitList.length; j++){
-        for(let i = 0; i <= limitList[j]; i++) {
-            setTimeout(function () {
-                document.getElementsByClassName("value1")[j].innerHTML = i + "%";
-            }, SPEED * i);
-        }
-    }
-}
-
-increase([7,78,8]);
